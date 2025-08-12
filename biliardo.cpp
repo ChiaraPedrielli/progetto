dbe255b458998c73ea513b990e5a6282172ce508
chiaraperoni <chiara.peroni99@gmail.com>
2025-08-12 20:29:13 +0200

    modifiche da chiara peroni il 12/08

--- biliardo.cpp
+++ biliardo.cpp
@@ -1,15 +1,71 @@
 //implementazioni dei metodi
 #include "biliardo.hpp"
 #include <stdexcept>
+#include <cmath>
 
 //c'erano dei nomi che non si possono dare ai namespace
 namespace pf{
+
+    //overload operatore == tra due punti
+    bool operator==(Point const& lhs, Point const& rhs) {
+        return lhs.x == rhs.x && lhs.y == rhs.y;
+    }
+
+    //metodi getter per poter accedere alle variabili membro di Ball:
+    const Point& Ball::coordba() const { return coordba_; };
+    double Ball::d() const { return d_; };
+
+    void Ball::move_to(Point new_point) {
+        coordba_ = new_point;
+    };
+
+    /*
     double Border::slope(Point const& p1, Point const& p2){
         if (p1.x == p2.x){
             throw std::runtime_error ("La retta è veticale");
-        }else{return double ((p2.y-p1.y)/(p2.x-p1.x));}
+        }
+        else {
+            return double ((p2.y-p1.y)/(p2.x-p1.x));
+        }
         
     }
+    */
+
+      const CollisionResult Border::next_collision(Ball const& b) {
+        double s = std::tan(b.d());
+        double x_up = (((r1_) + s*((b.coordba()).x) - (b.coordba()).y) / (s - slopeup_));
+
+        if (x_up > 0) {
+            if (x_up <= L_) {
+            bool hit = true;
+            bool up = true; 
+            double y_up = r1_ + (slopeup_)*x_up;
+            }
+
+            else {
+                //vuol dire che la palla è uscita direttamente senza urtare
+                bool hit = false;
+            }
+        }
+
+        else {
+           double x_down = ((-(r1_) + s*((b.coordba()).x) - (b.coordba()).y) / (s + slopeup_));
+
+            if (x_up <= L_) {
+            bool hit = true;
+            bool up = false; 
+            double y_down = -r1_ - (slopeup_)*x_down;
+            }
+
+            else {
+                //vuol dire che la palla è uscita direttamente senza urtare
+                bool hit = false;
+            }
+        }
+
+     }
+
+    
 
     void Bounce(Border& r, Ball& b );
 }
--- biliardo.hpp
+++ biliardo.hpp
@@ -1,40 +1,64 @@
-//qua vengono dichiaarte le classi/struct con dento i rispettivi metodi, anche i metodi qui sono solo dichiarati,
-//  la loro implementazione va in .cpp
-//va tutto in un NAMESPACE
+// qua vengono dichiaarte le classi/struct con dento i rispettivi metodi,
+// anche i metodi qui sono solo dichiarati, la loro implementazione va in .cpp
+// va tutto in un NAMESPACE
 
 #ifndef PF_BILIARDO_HPP
 #define PF_BILIARDO_HPP
 
-namespace pf{
+namespace pf {
 
-struct Point{ //per le coordinate
-    double x;
-    double y;
+struct Point
+{ // per le coordinate
+  double x;
+  double y;
 };
 
-struct Border{
-    Point coordbo1; //coordinate estremi rette
-    Point coordbo2;
+// overload dell'operatore == se può servire per confrontare due punti
+bool operator==(Point const&, Point const&);
 
-    double slope(Point const& p1, Point const& p2);
+class Ball
+{
+ private:
+  Point coordba_{0., 0.};
+  double d_{0.}; // angolo di lancio
+
+ public:
+  Ball(Point p, double t) : coordba_{p} , d_{t} {} // costruttore, forse si può inserire gia qui una exception se l'angolo o le coordinate non sono valide
+
+  // metodi getter per poter accedere alle variabili membro:
+  const Point& coordba() const;
+  double d() const;
+
+  // metodi per aggiornare la posizione e la pendenza della retta dopo ogni urto
+  void move_to(Point new_point); //quindi poi una volta che calcoliamo la x dell'urto calcoliamo anche la y e poi convertiamo in un Point
+
+  void Bounce(Border& r, Ball& b);
+};
+
+struct CollisionResult {
+    bool has_hit;
+    Ball hit;
+    bool upper;
+};
+
+class Border
+{
+ private:
+  double r1_{0.}; // coordinate estremi rette
+  double r2_{0.};
+  double L_{0.};
+  double slopeup_{0.};
+
+  public:
+  Border (double R1, double R2, double lenght) : r1_{R1}, r2_{R2}, L_{lenght}, slopeup_{(r2_ - r1_)/L_} {}
+  //double slope(Point const& p1, Point const& p2);
+  
+
+  const CollisionResult next_collision(Ball const& b);
+
 
 };
 
-struct Ball{
-    Point coordba;
-    double d; //angolo di lancio
-
-    void Bounce(Border& r, Ball& b );
-};
-
-
-
-
-
-
-}//fine namespace pf
-
-
-
+} // namespace pf
 
 #endif
