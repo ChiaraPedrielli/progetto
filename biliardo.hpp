0fa399872bd5ec22fcfebe4e6a056ff64cda0611
chiaraperoni <chiara.peroni99@gmail.com>
2025-08-23 13:08:13 +0200

    tolto alcuni commenti, messo in inglese e fatto test

--- biliardo.hpp
+++ biliardo.hpp
@@ -1,22 +1,15 @@
-// qua vengono dichiaarte le classi/struct con dento i rispettivi metodi,
-// anche i metodi qui sono solo dichiarati, la loro implementazione va in .cpp
-// va tutto in un NAMESPACE
-
 #ifndef PF_BILIARDO_HPP
 #define PF_BILIARDO_HPP
 #include <SFML/Graphics.hpp>
 
 namespace pf {
 
-struct Point { // per le coordinate
+struct Point {
   double x;
   double y;
 };
 
-// overload dell'operatore == se può servire per confrontare due punti
-bool operator==(Point const &, Point const &);
-
-// froward declaration per evitare problemi di ordine di dichiarazione
+//Forward declaration
 class Ball;
 class Border;
 struct CollisionResult;
@@ -24,36 +17,34 @@ struct CollisionResult;
 class Ball {
 private:
   Point coordba_{0., 0.};
-  double d_{0.}; // angolo di lancio
+  double d_{0.};            //launch angle
 
 public:
   Ball(Point p, double t)
       : coordba_{p}, d_{t} {
-  } // costruttore, forse si può inserire gia qui una exception se l'angolo o le
-    // coordinate non sono valide
+  }
 
-  // metodi getter per poter accedere alle variabili membro:
+  //Getter methods to access member variables
   const Point &coordba() const;
   double d() const;
 
-  // metodi per aggiornare la posizione e la pendenza della retta dopo ogni urto
-  void move_to(
-      Point new_point); // quindi poi una volta che calcoliamo la x dell'urto
+  //Methods for updating the position and slope of the straight line after each impact
+  void move_to(Point new_point); // quindi poi una volta che calcoliamo la x dell'urto
                         // calcoliamo anche la y e poi convertiamo in un Point
 
-  void Bounce(Border &r, Ball &b);
   void set_angle(double new_s);
 };
 
 struct CollisionResult {
-  bool has_hit;
+  bool has_hit;           //True if the collision occurs
   Ball hit;
-  bool upper;
+  bool upper;             //True if the collision occurs with the upper border
 };
+
 
 class Border {
 private:
-  double r1_{0.}; // coordinate estremi rette
+  double r1_{0.}; 
   double r2_{0.};
   double L_{0.};
   double slopeup_{0.};
@@ -62,11 +53,12 @@ public:
   Border(double R1, double R2, double lenght)
       : r1_{R1}, r2_{R2}, L_{lenght}, slopeup_{(r2_ - r1_) / L_} {}
 
-  // metodi getter
+  //Getter methods
   double r1() const;
   double r2() const;
   double L() const;
   double slopeup() const;
+
   void move_border(double r1, double r2, double L);
   static void initial_checks(Border const &b1, Border const &b2,
                              Ball const &ball);
@@ -88,12 +80,10 @@ struct Result {
   Result(int bo, const Ball &ba, std::vector<Ball> &tr)
       : bounces(bo), result(ba), trajectory(tr) {}
 
-  // QUESTE QUI SOTTO SONO LE DUE VERSIONI DELLA FUNZIONE UNA CIN SGML COMPLETA
-  // E UNA SENZA SFML CHE PUO ESSERE USATA PER I TEST
   static Result BallSimulation(Border &b1, Border &b2, Ball &b);
   
 };
 
-} // namespace pf
+}
 
 #endif
--- biliardo_test.cpp
+++ biliardo_test.cpp
@@ -2,9 +2,6 @@
 #include "biliardo.hpp"
 #include "doctest.h"
 
-// test da fare:
-//  1) che le rette non siano verticali
-//  2) che la palla sia tra le due rette e non fuori
 TEST_CASE("Testing BallSimulation") {
   pf::Ball ball({0, 0}, 0);
   pf::Border b1(0, 0, 1);
@@ -30,8 +27,6 @@ TEST_CASE("Testing BallSimulation") {
     CHECK_THROWS(pf::Result::BallSimulation(b1, b2, ball));
   }
 
-  // la palla entra correttamente, con un angolo di 30 gradi ma poi a un certo
-  // punto torna indietro
   SUBCASE("Ball enters cooreclty but then comes back") {
     ball.move_to({0, 310});
     ball.set_angle(0.524);
@@ -40,7 +35,6 @@ TEST_CASE("Testing BallSimulation") {
     CHECK_THROWS(pf::Result::BallSimulation(b1, b2, ball));
   }
 
-  // la palla entra correttamente ed esce subito senza toccare i bordi
   SUBCASE("Ball comes out without touching the borders") {
     ball.move_to({0, 310});
     ball.set_angle(0.524);
@@ -52,7 +46,6 @@ TEST_CASE("Testing BallSimulation") {
     CHECK(res.bounces == 0);
   }
 
-  // la palla entra correttamente ed esce dopo 1 rimbalzo
   SUBCASE("Ball comes out after one bounce") {
     ball.move_to({0, 340});
     ball.set_angle(0.785);
@@ -60,13 +53,10 @@ TEST_CASE("Testing BallSimulation") {
     b2.move_border(200, 215, 50);
     pf::Result res = pf::Result::BallSimulation(b1, b2, ball);
     CHECK(res.result.coordba().y == doctest::Approx(367.586).epsilon(0.05));
-    // CHECK(res.bounces == doctest40
-    //::Approx(1.).epsilon(0.05));
     CHECK(res.bounces == 1);
     CHECK(res.result.d() == doctest::Approx(-1.3679).epsilon(0.05));
   }
 
-  // la palla entra correttamente ed esce dopo 2 rimbalzi
   SUBCASE("Ball comes out after 2 bounces") {
     ball.move_to({0, 330});
     ball.set_angle(-1.100);
@@ -74,9 +64,36 @@ TEST_CASE("Testing BallSimulation") {
     b2.move_border(220, 230, 98);
     pf::Result res = pf::Result::BallSimulation(b1, b2, ball);
     CHECK(res.result.coordba().y == doctest::Approx(292.111).epsilon(0.05));
-    // CHECK(res.bounces == doctest::Approx(2.).epsilon(0.05));
     CHECK(res.bounces == 2);
     CHECK(res.result.d() == doctest::Approx(-1.50675).epsilon(0.05));
+  }
+
+  SUBCASE("Ball comes back p2") {
+    ball.move_to({0, 320});
+    ball.set_angle(1.220);
+    b1.move_border(400, 380, 130);
+    b2.move_border(200, 220, 130);
+    CHECK_THROWS(pf::Result::BallSimulation(b1, b2, ball));
+  }
+
+  SUBCASE("r2>r1") {
+    ball.move_to({0, 350});
+    ball.set_angle(1.0472);
+    b1.move_border(370, 390, 60);
+    b2.move_border(230, 210, 60);
+    pf::Result res = pf::Result::BallSimulation(b1, b2, ball);
+    CHECK(res.result.coordba().y == doctest::Approx(349.30).epsilon(0.05));
+    CHECK(res.result.d() == doctest::Approx(-0.4037).epsilon(0.05));
+  }
+
+  SUBCASE("r2>r1 and negative angle") {
+    ball.move_to({0, 290});
+    ball.set_angle(-1.120);
+    b1.move_border(370, 390, 60);
+    b2.move_border(230, 210, 60);
+    pf::Result res = pf::Result::BallSimulation(b1, b2, ball);
+    CHECK(res.result.coordba().y == doctest::Approx(231.55).epsilon(0.05));
+    CHECK(res.result.d() == doctest::Approx(0.4765).epsilon(0.05));
   }
 }
 
@@ -87,10 +104,8 @@ TEST_CASE("Testing initial_checks") {
   pf::Border b2(0, 0, 1);
   pf::Result end(0, ball);
 
-  // ANGOLO CHE ANDREBBE ALL'INDIETRO (MAGGIORE DI 90 GRADI)
-  SUBCASE("Not accepetd angle") {
+  SUBCASE("Not accepetd angle >90°") {
     ball.set_angle(1.57);
-
     CHECK_THROWS(pf::Border::initial_checks(b1, b2, ball));
   }
 
@@ -98,6 +113,13 @@ TEST_CASE("Testing initial_checks") {
     ball.move_to({0, 578});
     b1.move_border(480, 580, 1409);
     b2.move_border(120, 20, 1409);
+    CHECK_THROWS(pf::Border::initial_checks(b1, b2, ball));
+  }
+
+  SUBCASE("Negative L") {
+    ball.move_to({0, 578});
+    b1.move_border(480, 580, -50);
+    b2.move_border(120, 20, -50);
     CHECK_THROWS(pf::Border::initial_checks(b1, b2, ball));
   }
 
